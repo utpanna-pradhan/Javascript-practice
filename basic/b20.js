1e447a036f7313de2cd1406a34e2af3d91e9bc6a
Utpanna Pradhan <Utpanna828@gmail.com>
2025-06-08 23:22:43 +0530

    added one basic question

--- basic/b20.js
+++ basic/b20.js
@@ -1,1 +1,11 @@
 // Find the sum of digits of a number
+function sumofdigit(num){
+    let sum=0;
+    const newstr=num.toString();
+    for(let i=0;i<newstr.length;i++){
+        sum+=parseInt(newstr[i]);
+       
+    }
+     return sum;
+}
+console.log(sumofdigit(1234));
